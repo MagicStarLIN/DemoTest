1140d8335e79cef31e921ba2108afff32f3d0812
刘常林 <751969316@qq.com>
2020-01-22 20:07:30 +0800

    add dependency common-lang3

---
+++ src/main/java/com/lcl/entity/LxrMore.java
@@ -0,0 +1,30 @@
+package com.lcl.entity;
+
+import lombok.Data;
+
+/**
+ * @author liuchanglin
+ * @version 1.0
+ * @ClassName: LxrMore
+ * @date 2020/1/16 1:50 下午
+ */
+@Data
+public class LxrMore {
+    private Integer id;
+    private String dwmc;
+    private String lxr;
+    private String addr;
+    private String phone;
+    private String mobile;
+    private String email;
+    private String zhiwei;
+    private String fax;
+    private String sex;
+    private String beizhu;
+    private Integer type;
+    private Integer xmid;
+    private String isJob;
+    private String isNew;
+    private Integer flag;
+    private String postCode;
+}
